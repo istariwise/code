03ab4edcee7b3fe6284277e72d8a7ed09f38554a
istariwise <147575254+istariwise@users.noreply.github.com>
2023-12-19 16:30:48 +0800

    Create magic_ono.c

---
+++ magic_ono.c
@@ -0,0 +1,43 @@
+//仿照老師上課內容
+#ifndef _MAGIC_H_
+#define _MAGIC_H_
+
+#define M_SIZE 10
+
+#define EQU(p,q)  (p.x==q.x && p.y==q.y)
+
+//#define x NULL
+
+//座標結構  _t表結構
+typedef struct axis {
+    int x;
+    int y;
+} axis_t;
+
+//堆疊結構
+typedef struct stack {
+    axis_t elements[M_SIZE*M_SIZE];  //10X10的矩陣，每一格都是座標結構
+    int top;                         //top 堆疊陣列中指著的最後一個欄位的號碼or指標
+}stack_t;
+
+typedef struct stack1 {
+    axis_t *elements;  //跟隔壁同一個名稱？
+    int top;           //top 堆疊陣列中指著的最後一個欄位的號碼or指標
+}stack1_t;
+
+typedef struct axis1 {
+    axis_t p;
+    struct axis1 *next;
+} stackP_t;
+
+//將其他.c檔的副程式名稱放進來
+void push(stack_t *stack, axis_t axis);
+axis_t pop(stack_t *stack);
+int isEmpty(stack_t *stack);
+
+static inline int equ(axis_t *a , axis_t *b){
+    return (a->x == b->x && a->y == b->y);
+}
+
+
+#endif
