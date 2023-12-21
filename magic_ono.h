835a899eb263ab77aef8a50b244261b234d315db
istariwise <147575254+istariwise@users.noreply.github.com>
2023-12-22 00:53:21 +0800

    Update magic_ono.h

--- magic_ono.h
+++ magic_ono.h
@@ -1,9 +1,12 @@
-//仿照老師上課內容 ， 做修改
+//仿照老師上課內容, 做修改
 #ifndef _MAGIC_H_
 #define _MAGIC_H_
+
 #define M_SIZE 10
+
 #define EQU(p,q)  (p.x==q.x && p.y==q.y)
 
+//#define x NULL
 
 //座標結構  _t表結構
 typedef struct axis {
@@ -17,10 +20,18 @@ typedef struct stack {
     int top;                         //top 堆疊陣列中指著的最後一個欄位的號碼or指標
 }stack_t;
 
+
 //將其他.c檔的副程式名稱放進來
 void push(stack_t *stack, axis_t axis);
 axis_t pop(stack_t *stack);
 int isEmpty(stack_t *stack);
 
-#endif
+//static inline 放在很短的副程式前，用意讓compile決定太短可變成巨集非副程式方式，避免使用系統堆疊
+//跟define一樣是巨集
+//static 用途是將變數變成全域變數，且只有該區塊可以存取
+static inline int equ(axis_t *a , axis_t *b){
+    return (a->x == b->x && a->y == b->y);
+}
 
+
+#endif
