8926aa8e83a575f1e0b65c7798e5c2e14eab4335
istariwise <147575254+istariwise@users.noreply.github.com>
2023-12-22 22:54:54 +0800

    Update CLibraryPractice.c

--- CLibraryPractice.c
+++ CLibraryPractice.c
@@ -1,3 +1,4 @@
+//老師好，之前在趕論文final，抱歉現在開始追。
 #include <math.h>
 #include <assert.h>
 #include <ctype.h>
@@ -7,6 +8,7 @@
 #include <string.h>
 #include <time.h>
 
+// double sqrt(double x) / double pow(double x, double y) 兩點座標的距離
 void distancexy(){
 
     int x1, y1, x2, y2;
@@ -21,8 +23,75 @@ void distancexy(){
 
 }
 
+// double fmod(double x, double y)
+void evenOrOdd(){
+    int x;
+    int remainder;
+    printf("please enter a number.\n");
+    scanf("%d",&x);
+
+    remainder = (int)fmod((double)x , 2);
+
+    if(remainder == 0)
+        printf("The number %d is even\n", (int)x);
+    else
+        printf("The number %d is odd.\n", (int)x);
+}
+
+//double ceil(double x) 無條件進位到整數第N位(進位到個位，N=1；十位,N=2；百位,N=3)
+void roundUP(){
+    int x;
+    int n;
+    double y;
+    printf("please enter a number.\n");
+    scanf("%d",&x);
+    printf("Is rounding up to N position?\n");
+    scanf("%d",&n);
+
+    y = x/(pow(10 , (n - 1)));
+    y = ceil(y);
+    y = y * (pow(10 , (n - 1)));
+    printf("Round up to %d position.\n", n);
+    printf("%d is rounded up to %d", x, (int)y);
+}
+
+//double floor(double x) 無條件捨去到整數第N位(捨去到個位，N=1；十位,N=2；百位,N=3)
+void roundDown(){
+    int x,n;
+    double y;
+    printf("please enter a number.\n");
+    scanf("%d",&x);
+    printf("Is Rounding Down to N position?\n");
+    scanf("%d",&n);
+
+    y = x/(pow(10 , (n - 1)));
+    y = floor(y);
+    y = y * (pow(10 , (n - 1)));
+    printf("Round down to %d position.\n", n);
+    printf("%d is rounded down to %d", x, (int)y);
+}
+
+//double fabs(double x) 柯西不等式
+void Cauchy(){
+    int a,b,c,d;
+    double y1,y2;
+    printf("please enter four number to calculate Cauchy's inequality.\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    y1 = sqrt(pow(a,2) + pow(b,2)) * sqrt(pow(c,2) + pow(d,2));
+    y2 = fabs(a * c + b * d);
+    if(y1 >= y2)
+        printf("As %f >= %f ,The result of Cauchy's inequality hold \n", y1,y2);
+    else
+        printf("As %f < %f ,The result of Cauchy's inequality don't hold \n", y1,y2);
+}
+
+
+
 void main(){
 
-    distancexy();
-
+    //distancexy();
+    //evenOrOdd();
+    //roundDown();
+    //roundUP();
+    Cauchy();
 }
